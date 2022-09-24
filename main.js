53dfcb1bee1f0d943bf177850c8a382a99065677
Carlos Diego Tavares <102318991+diego-tavares@users.noreply.github.com>
2022-09-24 16:10:43 -0300

    funções para gerar os caracteres aleatórios
    utilizado random.Math para randomizar a escolha dentro da lista

--- main.js
+++ main.js
@@ -4,3 +4,29 @@ checkbox.addEventListener('change', ()=>{
   document.body.classList.toggle('dark');
 })
 
+const randomFunction = {
+  lower: randomLower,
+  upper: randomUpper,
+  number: randomNumber,
+  symbol: randomSymbol,
+}
+
+function randomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function randomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function randomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function randomSymbol() {
+  listSymbol = ['!', '@', '#', '$', '%', '^', '&', '(', ')', '_', '-', '+', '=', '{', '[', '}', ']', '?'];
+  symbol = listSymbol[Math.floor(Math.random() * listSymbol.length)];
+  return symbol;
+}
+
+console.log(randomSymbol())
